47aa64af1afd9fa2e9c8c2587a50aac1520585c9
Mak31m232 <max.lebedev1104@gmail.com>
2025-02-13 12:06:21 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,15 +17,94 @@ let form = 0;
 
 function input() {
     if (form==0){
-        let input = document.getElementById("form")
-        input.style.marginLeft = "11%"
-        input.style.opacity  = "1"
+        let form_input = document.getElementById("form")
+        let input = document.getElementById("input")
+        input.style.width = "450px"
+        form_input.style.marginLeft = "-36%"
+        form_input.style.opacity  = "1"
+        form_input.style.zIndex  = "4"
         form = 1;
     }
     else if (form==1){
-        let input = document.getElementById("form")
-        input.style.marginLeft = "45%"
-        input.style.opacity  = "0"
+        let form_input = document.getElementById("form")
+        let input = document.getElementById("input")
+        form_input.style.marginLeft = "35%"
+        form_input.style.opacity  = "0"
+        form_input.style.zIndex  = "2"
+        input.style.width = "150px"
         form = 0;
+    }
+}
+
+function cross_input() {
+    let input = document.getElementById("form")
+    input.style.opacity  = "0"
+    input.style.zIndex  = "2"
+    form = 0;
+}
+
+const registration = () => {
+    let username = document.getElementById("usernameField-username").value
+    let password = document.getElementById("usernameField-password").value
+    let registration = document.getElementById("registration-main")
+    const user = {
+        name: username,
+        password: password
+    }
+    if((username.length == 0)||(password.length == 0)){
+        let input_username = document.getElementById("usernameField-username")
+        let input_password = document.getElementById("usernameField-password")
+        input_username.style.borderColor = "red"
+        input_password.style.borderColor = "red"
+        alert("Вы не заполнили поля(-е)")
+    }
+    else if ((username.length <= 2)||(password.length <= 5)) {
+        let input_username = document.getElementById("usernameField-username")
+        let input_password = document.getElementById("usernameField-password")
+        input_username.style.borderColor = "red"
+        input_password.style.borderColor = "red"
+        alert("Пороль должен быть не меньше 5 символов, а имя не меньше 2")
+    }
+    else {
+        alert("Вы успешно зарегестрировались")
+        console.log(user)
+        registration.style.display = "none"
+    }
+}
+
+function right_switch() {
+    let right_switch = document.getElementById("ul-menu")
+    let svg_switch = document.getElementById("bi-bi-chevron-left")
+    let svg_switch_right = document.getElementById("bi-bi-chevron-right")
+    let input = document.getElementById("input")
+    input.style.width = "400px"
+    svg_switch_right.style.display = "none"
+    svg_switch.style.display = "block"
+    right_switch.style.marginLeft = "34%"
+}
+
+function left_switch() {
+    let left_switch = document.getElementById("ul-menu") 
+    let svg_switch = document.getElementById("bi-bi-chevron-left")
+    let svg_switch_right = document.getElementById("bi-bi-chevron-right")
+    let input = document.getElementById("input")
+    left_switch.style.marginLeft = "40%"
+    input.style.width = "450px"
+    svg_switch_right.style.display = "block"
+    svg_switch.style.display = "none"
+}
+
+let shet_menu = 0;
+
+function menu() {
+    if(shet_menu==0){
+        let menu = document.getElementById("ul-menu")
+        menu.style.marginLeft = "-160%"
+        shet_menu = 1;
+    }
+    else if(shet_menu==1){
+        let menu = document.getElementById("ul-menu")
+        menu.style.marginLeft = "-70%"
+        shet_menu = 0;
     }
 }
